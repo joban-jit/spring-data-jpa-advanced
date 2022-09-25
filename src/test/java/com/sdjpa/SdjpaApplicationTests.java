ff232fb908bab713d05789fd58780a36853ee503
Jobanjit Singh <jbnjeet@gmail.com>
2022-09-25 10:19:28 +0530

    initial commit

---
+++ src/test/java/com/sdjpa/SdjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
