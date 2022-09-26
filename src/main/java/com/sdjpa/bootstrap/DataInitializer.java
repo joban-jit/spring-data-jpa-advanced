ee3221636a19137acffeb3a83c010239e0dd8408
Jobanjit Singh <jbnjeet@gmail.com>
2022-09-26 09:43:31 +0530

    added h2 db

--- src/main/java/com/sdjpa/bootstrap/DataInitializer.java
+++ src/main/java/com/sdjpa/bootstrap/DataInitializer.java
@@ -1,5 +1,6 @@
 package com.sdjpa.bootstrap;
 
+import com.sdjpa.domain.Book;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,12 +13,19 @@ import lombok.AllArgsConstructor;
 public class DataInitializer implements CommandLineRunner {
 
 	private final BookRepository bookRepository;
-	
-	
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-		// TODO Auto-generated method stub
+		Book book000 = new Book("Domain Driven Design", "123", "RandomHouse");
+		Book saved000 = bookRepository.save(book000);
+		System.out.println("Id: "+book000.getId());
+		Book book001 = new Book("Spring in Action", "234343", "Oriely");
+		Book saved001 = bookRepository.save(book001);
+		System.out.println("Id: "+book001.getId());
+		bookRepository.findAll().forEach(book->{
+			System.out.println("Book id: "+book.getId());
+			System.out.println("Book Title: "+book.getTitle());
+		});
 
 	}
 
