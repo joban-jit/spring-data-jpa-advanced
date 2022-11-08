eaa11d8497ab97d3afe5df13e5adf34b4acbd7af
Jobanjit Singh <jbnjeet@gmail.com>
2022-11-08 22:40:59 +0530

    spring data jpa repo configs and disabled liquibase for now

--- src/main/java/com/sdjpa/config/CardHolderDatabaseConfiguration.java
+++ src/main/java/com/sdjpa/config/CardHolderDatabaseConfiguration.java
@@ -8,13 +8,18 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
 
 import javax.sql.DataSource;
-
+@EnableJpaRepositories(
+        basePackages = "com.sdjpa.repositories.creditcardholder",
+        entityManagerFactoryRef = "cardHolderEntityManagerFactory",
+        transactionManagerRef = "cardHolderTransactionManager"
+)
 @Configuration
 public class CardHolderDatabaseConfiguration {
 
