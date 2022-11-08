5e240f5a4148f6dab778b3f3ac99e53ab909aff0
Jobanjit Singh <jbnjeet@gmail.com>
2022-11-08 22:09:32 +0530

    data source connections

--- src/main/java/com/sdjpa/config/PanDatabaseConfiguration.java
+++ src/main/java/com/sdjpa/config/PanDatabaseConfiguration.java
@@ -1,16 +1,33 @@
 package com.sdjpa.config;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class PanDatabaseConfiguration {
 
+    @Primary
     @Bean
     @ConfigurationProperties("spring.pan.datasource")
     public DataSourceProperties panDataSourceProperties(){
         return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    public DataSource panDataSource(
+            @Qualifier("panDataSourceProperties") DataSourceProperties panDataSourceProperties
+    )
+    {
+        return panDataSourceProperties.initializeDataSourceBuilder()
+                .type(HikariDataSource.class)
+                .build();
     }
 }
